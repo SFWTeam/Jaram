60e75245a11f55280a57f4e46efb11e0e2b2689e
Ege K <52561965+GianC-Dev@users.noreply.github.com>
2020-03-12 20:10:42 +0300

    demo start.py

---
+++ start.py
@@ -0,0 +1,13 @@
+from src.main.Jaram.VersionInfo import version
+
+print('--------------------------------')
+print('Welcome The {}'.format(version[0]))
+print('Creator: GianC-Dev & SFW-Team')
+print('--------------------------------')
+print('{} A MCPE Software'.format(version[0]))
+print('Starting MCPE {} Protocol'.format(version[2]))
+print('--------------------------------')
+print('Great Peridot!')
+print('--------------------------------')
+pass
+
